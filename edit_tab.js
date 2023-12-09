ba21a60b1948225f17f999f66aafc37a0f1dd49d
Joseph <141452587+flvent-englishman@users.noreply.github.com>
2023-12-09 12:18:02 -0500

    Create edit_tab.js

---
+++ edit_tab.js
@@ -0,0 +1,16 @@
+javascript:(function(){
+        const title = prompt('Please put in the name you want to rename your tab to.');
+        if (title != null || title != '') {
+            document.title = title;
+        }
+        const favicon = prompt('Please put in an image link to change the favicon to.');
+        if (favicon != null || favicon != '') {
+            let icon = document.querySelector('link[rel~="icon"]');
+            if (!icon) {
+                icon = document.createElement('link');
+                icon.rel = 'icon';
+                document.head.appendChild(icon);
+            }
+            icon.href = favicon;
+        }
+    })();
