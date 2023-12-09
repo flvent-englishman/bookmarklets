ff99a88cfd2e527ff9668955f2485dfe21e030f1
Joseph <141452587+flvent-englishman@users.noreply.github.com>
2023-12-09 12:43:04 -0500

    Create glitch_tab.js

---
+++ Tab Related/glitch_tab.js
@@ -0,0 +1,9 @@
+javascript:(function(){
+  let msg = prompt('What do you want the message to be? Leave blank for the default.');
+  if (msg == '') {
+    msg = 'Warning:file//directory-not-found@...ZOV//Exit Status:441||559;err(type:"catch"){Local directory or file not found => parseInt(*kaOS)}"FAILURE TO EXECUTE"12:01:56=11/06/21,SET000'
+  }
+  while(true){
+    alert(msg);
+  }
+})();
