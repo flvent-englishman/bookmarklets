72935e4aa6511f615466d95dd6ccafcfb7f013ab
Joseph <141452587+flvent-englishman@users.noreply.github.com>
2024-02-03 17:39:14 -0500

    Update and rename edit_tab.js to cloak_tab.js

--- Tab Related/cloak_tab.js
+++ Tab Related/cloak_tab.js
@@ -1,10 +1,10 @@
 javascript:(function(){
         const title = prompt('Please put in the name you want to rename your tab to.');
-        if (title != null || title != '') {
+        if (title != null && title != '') {
             document.title = title;
         }
         const favicon = prompt('Please put in an image link to change the favicon to.');
-        if (favicon != null || favicon != '') {
+        if (favicon != null && favicon != '') {
             let icon = document.querySelector('link[rel~="icon"]');
             if (!icon) {
                 icon = document.createElement('link');
